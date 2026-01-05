78ed7c822ac11f453b8e34b7561524d4eb39eb19
Ivan227-hub <i.kushkov-de21@lll.kpi.ua>
2026-01-05 13:32:13 +0200

    edited work

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000" />
+        <link rel="icon" href="/favicon.ico" /> 
+        <title>NoteHub</title>
+      </head>
       <body>
         <TanStackProvider>
           <Header />
--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,28 +1,23 @@
+// page.tsx
 import { notFound } from "next/navigation";
 import { dehydrate, QueryClient } from "@tanstack/react-query";
 import { fetchNoteById } from "@/lib/api";
 import NoteDetailsClient from "./NoteDetails.client";
 
-interface NotePageProps {
-  params: { id: string };
-}
-
-export default async function NotePage({ params }: NotePageProps) {
+export default async function NotePage({ params }: { params: { id: string } }) {
   const queryClient = new QueryClient();
 
   try {
+    const note = await fetchNoteById(params.id);
+    if (!note?.id) notFound(); // вызываем только если нет заметки
+
     await queryClient.prefetchQuery({
       queryKey: ["note", params.id],
       queryFn: () => fetchNoteById(params.id),
     });
   } catch {
-    notFound(); 
+    notFound(); // если fetch упал (404)
   }
 
-  return (
-    <NoteDetailsClient
-      id={params.id}
-      dehydratedState={dehydrate(queryClient)}
-    />
-  );
+  return <NoteDetailsClient id={params.id} dehydratedState={dehydrate(queryClient)} />;
 }
