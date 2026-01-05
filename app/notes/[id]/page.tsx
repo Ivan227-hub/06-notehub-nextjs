adc39da02d2a64fc753ce5e6bdc94f66046ce2b0
Ivan227-hub <i.kushkov-de21@lll.kpi.ua>
2026-01-05 14:09:45 +0200

    HW6

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,22 +1,27 @@
-// page.tsx
 import { notFound } from "next/navigation";
 import { dehydrate, QueryClient } from "@tanstack/react-query";
 import { fetchNoteById } from "@/lib/api";
 import NoteDetailsClient from "./NoteDetails.client";
 
-export default async function NotePage({ params }: { params: { id: string } }) {
+interface Props {
+  params: { id: string };
+}
+
+export default async function NotePage({ params }: Props) {
   const queryClient = new QueryClient();
 
   try {
-    const note = await fetchNoteById(params.id);
-    if (!note?.id) notFound(); // вызываем только если нет заметки
-
+    // prefetch заметки в кеш
     await queryClient.prefetchQuery({
       queryKey: ["note", params.id],
       queryFn: () => fetchNoteById(params.id),
     });
+
+    // проверка, есть ли заметка в кеше
+    const note = queryClient.getQueryData(["note", params.id]);
+    if (!note) notFound(); // если нет — 404
   } catch {
-    notFound(); // если fetch упал (404)
+    notFound(); // если fetch упал
   }
 
   return <NoteDetailsClient id={params.id} dehydratedState={dehydrate(queryClient)} />;
